c90de4a6abb60d3ba5432a51d5cb40af361c200a
Janice Pena <jipena94@gmail.com>
2019-05-23 02:36:11 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+  
+  attr_reader :title
+  
+  attr_accessor :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+  
+end
